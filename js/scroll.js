8b2ded1795493076bf1278604521d1f751784c33
Marta Para <mapa.startuj@gmail.com>
2018-02-23 22:15:30 +0100

    added animation for icons

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,17 @@
 $(function() {
 	if (screen.width > 768) {
 		$("#about-title").addClass("animated");
+		$(".all-icons").addClass("animated");
 		$("#portfolio-title").addClass("animated");
+		$("#carousel-example-generic").addClass("animated");
 		$("#contact").addClass("animated");
 		window.sr = ScrollReveal({ reset: true }); 
 		sr.reveal('.animated');
 	} else {
 		$("#about-title").removeClass("animated");
+		$(".all-icons").removeClass("animated");
 		$("#portfolio-title").removeClass("animated");
+		$("#carousel-example-generic").removeClass("animated");
 		$("#contact").removeClass("animated");
 	}
 });
